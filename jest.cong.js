1d7b5a874a08222255d5a7e88a4e30e1d74972db
Mamadaines <adaines7@gmail.com>
2021-01-19 16:34:21 -0800

    json and jest changes

--- jest.cong.js
+++ jest.cong.js
@@ -1,5 +1,8 @@
 module.exports = {
     preset: 'ts-jest',
-    testEnvironment: 'node',
-    transformIgnorePatterns: ["/node_modules/", "src/tests/pageObjects/"],
-};
+    globals: {
+      'ts-jest': {
+        diagnostics: false
+      }
+    }
+  };
